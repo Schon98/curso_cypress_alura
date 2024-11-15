aa7653cd116ac0f595a6c2fa1f72dc9df4fd1352
Schon98 <84647547+Schon98@users.noreply.github.com>
2024-11-15 09:22:10 -0300

    commit

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,15 +1,15 @@
-describe('Api Adopet',() =>{
-    const authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxYjc1NWJlMy00ZDJlLTQyYTAtODBjNC0zMzRkNThkMmE4NGEiLCJhZG9wdGVyTmFtZSI6Ik1hdGhldXMiLCJpYXQiOjE3MzE1NTAxOTIsImV4cCI6MTczMTgwOTM5Mn0.w-f_CKT3GJ2mEEOGQ1M01r3ZuBdQjVzvWkkOMMKg7Fs'
-
-    it('Mensagens da API', () => {
-        cy.request({
-            method: 'GET' ,
-            url:'https://adopet-api-i8qu.onrender.com/mensagem/1b755be3-4d2e-42a0-80c4-334d58d2a84a' ,
-            headers: {authorization}
-        }).then((res) => {
-            expect(res.status).to.be.equal(200)
-            expect(res.body).is.not.empty
-            expect(res.body).to.have.property('msg')
+describe('Api Adopet', () => {
+    //Atenção, o token possui um prazo de validade. Dessa forma, você deverá realizar o login e copiar o toker presente no Local Storage do seu navegador.     
+     it('Mensagens da API', () => {
+            cy.request({
+             method: 'GET' ,
+             url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
+             headers: Cypress.env()
+                   
+                }).then((res) => {
+                    expect(res.status).to.be.equal(200)
+                    expect(res.body).is.not.empty
+                    expect(res.body).to.have.property('msg')         
+                })
     })
-})
 })
